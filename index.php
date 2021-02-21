94d5a8e4e52adef1957718b06c31bac9d9a9972a
Michal Koperski <mkoperpno@gmail.com>
2021-02-21 22:05:02 +0100

    initializa project

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+require_once("src/Utils/debug.php");
+$test = 'test';
+?>
+
+<html>
+<head>
+
+</head>
+
+<body>
+    <h1>Witamy!!</h1>
+    <div>
+        <?php echo $test ?>
+    </div>
+</body>
+</html>
---
+++ src/Utils/debug.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(E_ALL); //nie ignoruje jakiegokolwiek błędu w trakcie pisania kodu
+ini_set('display_errors', '1'); //wyświetla wszystkie błędy
+
+function dump($data)
+{
+    echo '<div 
+    style="
+        display: inline-block;
+        padding: 0px 10px;
+        border: 1px solid gray;
+        background: lightgray;">
+<pre>';
+    print_r($data);
+    echo '</pre>
+</div>
+</br>';
+}
