084408f94de7fb915ef5a253c102dfa74724db9a
Michal Koperski <mkoperpno@gmail.com>
2021-02-28 20:10:54 +0100

    refactoring project to Object Programming

--- src/Utils/debug.php
+++ src/Utils/debug.php
@@ -11,7 +11,7 @@ function dump($data)
     style="
         display: inline-block;
         padding: 0px 10px;
-        border: 1px solid gray;
+        border: 1px dashed gray;
         background: lightgray;">
     <pre>';
         print_r($data);
